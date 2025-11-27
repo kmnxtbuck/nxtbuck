f56df5f6b272f0a3008a91adb23076e1141294d2
kmnxtbuck <karan-work@MacBook-Pro-3.local>
2025-11-27 02:59:08 -0500

    Add dummy about us

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,1 +1,13 @@
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "About Us",
+};
+
+export default function AboutUs() {
+  return (
+    <main className="px-8 py-12">
+      <h1 className="text-4xl font-bold text-white">About Us</h1>
+    </main>
+  );
+}
