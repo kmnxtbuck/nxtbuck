75e38f518c7539b40d3ca517b3806e52aeb0d0b2
kmnxtbuck <karan-work@MacBook-Pro-3.local>
2025-12-15 08:12:56 -0500

    Fix build errors

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 const createMDX = require("@next/mdx");
-const _remarkGfm = require("remark-gfm");
-const remarkGfm = _remarkGfm.default || _remarkGfm;
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm].filter(Boolean),
-  },
 });
 
 /** @type {import('next').NextConfig} */
